7aea36a249f4ee32e7c9efb36aa60add50e6cfd2
Sundeep Prakash Jaiswal <sundeeppjaiswal@hotmail.com>
2024-07-28 19:07:50 +0530

    Assignments
    Assignments

---
+++ 01. C# Language Basics/Assignments/AssignmentIfElse.cs
@@ -0,0 +1,36 @@
+﻿/*
+Write a program to findout "category of height" based on the given height of a person (in inches), using "if".
+Height categories:
+Height is less than 150 cm = "Dwarf"
+Height is between 150 cm and 165 cm = "Average height"
+Height is between 165 cm and 195 cm = "Tall"
+Height is above 195 cm = "Abnormal height"
+
+Note: 1 inch = 2.54 centimeter
+
+Eg:
+Input: 75
+Output: Tall
+*/
+class Program
+{
+    static void Main()
+    {
+        int height_in_inches = 75;  //input
+        double height_in_cm = height_in_inches * 2.54;  //1 inch = 2.54 centimeter
+        string result = null;  //initialize the 'result' variable
+
+        //check the height and find the height category accordingly
+        if (height_in_cm < 150)
+            result = "Dwarf";
+        else if (height_in_cm >= 150 && height_in_cm <= 165)
+            result = "Average height";
+        else if (height_in_cm > 165 && height_in_cm <= 195)
+            result = "Tall";
+        else
+            result = "Abnormal height";
+
+        System.Console.WriteLine(result); //Eg output: Tall
+        System.Console.ReadKey();
+    }
+}
---
+++ 01. C# Language Basics/Assignments/AssignmentLargestOfThreeNos.cs
@@ -0,0 +1,30 @@
+﻿/*
+Write a C//program to find out largest number among three numbers, using "if".
+Eg:
+num1 = 60
+num2 = 45
+num3 = 123
+Output: 123
+*/
+class Program
+{
+    static void Main()
+    {
+        //inputs
+        int num1 = 60, num2 = 45, num3 = 123;
+
+        int biggest;
+        if (num1 >= num2)  //"num1 is greater than num2"
+            if (num1 >= num3)  //"num1 is greater than num2" and "num1 is greater than num3"
+                biggest = num1;
+            else  //"num1 is greater than num2" and "num3 is greater than num1"
+                biggest = num3;
+        else  //"num2 is greater than num1"
+            if (num2 >= num3)  //"num2 is greater than num1" and "num2 is greater than num3"
+            biggest = num2;
+        else  //"num2 is greater than num1" and "num3 is greater than num2"
+            biggest = num3;
+        System.Console.WriteLine(biggest);
+        System.Console.ReadKey();
+    }
+}
