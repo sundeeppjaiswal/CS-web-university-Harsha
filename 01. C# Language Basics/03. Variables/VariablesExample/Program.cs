91d19d0d3fcb34f004f9269ce0c9e7b7392cfeeb
Sundeep Prakash Jaiswal <sundeeppjaiswal@hotmail.com>
2024-07-28 15:16:29 +0530

    Added my feature and adjusted the code
    Added my feature and adjusted the code.

--- 01. C# Language Basics/03. Variables/VariablesExample/Program.cs
+++ 01. C# Language Basics/03. Variables/VariablesExample/Program.cs
@@ -3,13 +3,13 @@
     static void Main()
     {
         //declare the variable
-        string studentName = "Scott";
-        int age = 20;
+        string studentName = "Sundeep";
+        int age = 30;
 
         //print value
         System.Console.Write("Hey ");
         System.Console.Write(studentName);
-        System.Console.Write(" , your age is ");
+        System.Console.Write(", your age is ");
         System.Console.Write(age);
 
         //wait for pressing some key on keyboard
