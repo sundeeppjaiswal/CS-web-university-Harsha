8f9ce0ce8c88cd9e2e7a5a8e61c667d253e5899b
Sundeep Prakash Jaiswal <sundeeppjaiswal@hotmail.com>
2024-07-29 11:47:44 +0530

    Reverse the number and logic of loops
    Reverse the number and logic of loops.

--- 01. C# Language Basics/12. Nested For loops/NestedFormExample/Program.cs
+++ 01. C# Language Basics/12. Nested For loops/NestedFormExample/Program.cs
@@ -2,9 +2,9 @@
 {
     static void Main()
     {
-        for (int i = 1; i <= 5; i++)
+        for (int i = 10; i >= 0; i--)
         {
-            for (int j = 1; j <= i; j++)
+            for (int j = 10; j >= i; j--)
             {
                 System.Console.Write(j);
                 System.Console.Write(" ");
